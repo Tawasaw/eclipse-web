90122fb485b89fcf432a3fa40797f1c720f93468
Faisal Abu-Nimeh <asd1815@gmail.com>
2026-09-07 23:07:11 -0700

    Use the peer-to-peer donation URL directly in the iframe

--- src/data/donation.ts
+++ src/data/donation.ts
@@ -1,8 +1,5 @@
 export const donationUrl =
   "https://www.zeffy.com/en-US/donation-form/support-eclipse-30618-build-a-strong-ftc-biobuzz-robot-2";
 
-export const donationEmbedPath = new URL(donationUrl).pathname.replace(
-  "/en-US/",
-  "/embed/"
-);
-export const donationEmbedUrl = new URL(donationEmbedPath, donationUrl).href;
+export const donationEmbedUrl =
+  "https://www.zeffy.com/en-US/peer-to-peer/support-eclipse-30618-build-a-strong-ftc-biobuzz-robot-2?donate=true";
